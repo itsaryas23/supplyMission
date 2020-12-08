5f87b357a34b9e857f88cc6e2bcff28d320aadeb
itsaryas23 <74662533+itsaryas23@users.noreply.github.com>
2020-12-08 22:02:36 +0530

    Add files via upload

---
+++ MyOwnLibrary.js
@@ -0,0 +1,15 @@
+function bounceOff(){
+    if(a.x - b.x < b.width/2 + a.width/2
+      && b.x - a.x < b.width/2 + a.width/2){
+  
+        a.velocityX = a.velocityX * (-1);
+        b.velocityX = b.velocityX * (-1);
+      }
+       
+      if(a.y - b.y < b.height/2 + a.height/2
+        && b.y - a.y < a.height/2 + b.height/2){
+    
+          a.velocityY = a.velocityY * (-1);
+          b.velocityY = b.velocityY * (-1);
+        }
+  }
